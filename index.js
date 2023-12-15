a286ce6582006f38221184b74d0b7022c123e417
Harshita Lakhchaura <harshitalakhchaura@gmail.com>
2023-12-15 10:52:23 +0530

    started work on backend

---
+++ index.js
@@ -0,0 +1,37 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const { connect } = require("http2");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*',
+        methoods: ['GET', 'POST']
+    }
+});
+
+app.use(cors());
+
+const PORT=process.env.PORT || 5000;
+
+app.get("/", (req, res) =>{
+    res.send('Server is running.');
+});
+
+io.on('connection',(socket)=>{
+    socket.emit('me',socket.id);
+
+    socket.on('disconnet',()=>{
+        socket.broadcast.emit('Call ended');
+    })
+
+    socket.on('callUser',(userToCall, signalData, from, name)=>{
+        io.to(userToCall).emit('Call user',{signal:signalData, from, name});
+    })
+
+    socket.io('answerCall',(data)=>{
+        io.to(data).emit('call accepted',data.signal);
+    })
+})
+
+server.listen(PORT,()=> console.log(`Server listening on port ${PORT}`));
